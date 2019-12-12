a7b9f19eccc3cfee04ba2fb6d7d98ec2e6a7f58a
Anthony Golovin and Uriel Gonzalez <anthony@>
2019-12-12 13:27:18 -0800

    Created Text adventure with player action methods and the initial game start

---
+++ Game/Game.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace TextGame.Game {
+//   class Game
+//   {
+//       //TBA should have all player actions might need to move inventory class into here?
+//   }
+
+  class Item
+  {
+      public string Spoon {get; set;} //Add maybe 5 Items EX. Key?
+      public string Hammer {get; set;}
+    
+      public Item(string spoon, string hammer)
+      {     
+      Spoon = spoon;
+      Hammer = hammer;
+      }
+      public string GetHammer()
+      {
+          return Hammer;
+      }
+      public void SetHammer(string newHammer)
+      {
+          Hammer = newHammer;
+      }
+      public string GetSpoon()
+      {
+          return Spoon;
+      }
+      public void SetSpoon(string newSpoon)
+      {
+          Spoon = newSpoon;
+      }
+    //   public string GetItem()
+    //   {
+          
+    //   }
+
+      
+    public static List<string> AddItem() //Possibly change to interact?
+    {
+        List<string> ItemList = new List<string>();
+        string pickUp = Console.ReadLine();//Check if you can rely on simply waiting or if you need a writeline before
+        if (pickUp.ToLower() == "pick up")
+        {
+            Console.WriteLine("Would you like to add item to inventory? (Y/N)");
+            string itemResponse = Console.ReadLine();
+            if (itemResponse.ToLower() == "y")
+            {
+                Console.WriteLine("Item was added to inventory");
+                ItemList.Add(itemResponse);
+                return ItemList;
+
+                // Item newItem = new Item();
+            }
+            else if (itemResponse.ToLower() == "n")
+            {
+                Console.WriteLine("Item was NOT added to inventory");
+                return ItemList;
+            }
+            else
+            {
+            Console.WriteLine("Please enter a valid Response EX. 'y' or 'n'");
+            return ItemList;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid Command");
+            return ItemList;
+        }
+    }
+    public void OpenInventory()
+    {
+        string inventoryCheck = Console.ReadLine();
+        if (inventoryCheck.ToLower() == "open inventory")
+        {
+            Console.WriteLine("These items are currently in your inventory:");// this looks like the wrong instance of inventory 
+        }
+    }
+  }
+  
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using TextGame.Game;
+
+namespace TextGame.Game {
+
+  public class Program
+  {
+      public static void Main()
+      {
+          List<Item> ItemList = new List<Item>();
+            AddItem();
+            OpenInventory();
+          Console.WriteLine(""); //Tutorial Explanation LONGGGGGGGGGGGG
+          Console.WriteLine("Would you like to begin your adventure?");
+          string gameStart = Console.ReadLine();
+          if (gameStart.ToLower() == "y")
+          {
+              
+              Console.WriteLine("You start in room with a door and a hammer on the ground");//EXPOSITION
+              AddItem();
+              bool playing = true;
+              while (playing)
+                    {
+                    //Call a method here called somethhing Like "game" filled with all of 
+                    //the other methods that contain player actions
+                    
+                    }
+          }
+          else if (gameStart == "n")
+          {
+                return;                   
+          }
+          else
+          {
+            Console.WriteLine("Please enter a valid answer EX.(y/n)");
+            Main();
+          }
+
+        //   string startGame = Console.ReadLine(); //EXPOSITION
+
+      }
+  }
+}
